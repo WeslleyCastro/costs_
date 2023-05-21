c194819dd4cae125c6884e7c0084aa3606f982b2
Weslley Castro da Silva <117310795+WeslleyCastro@users.noreply.github.com>
2023-05-21 16:17:11 -0300

    Update Project.js

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -20,7 +20,7 @@ function Project() {
     const [services, setServices] = useState([])
 
     useEffect(() => {
-        fetch(`http://localhost:5000/projects/${id}`, {
+        fetch(`https://json-cost.vercel.app/projects/${id}`, {
             method: "GET",
             headers: {
                 "Content-Type" : "application/json",
@@ -45,7 +45,7 @@ function Project() {
             return false
         }
 
-        fetch(`http://localhost:5000/projects/${project.id}`, {
+        fetch(`https://json-cost.vercel.app/projects/${project.id}`, {
             method: "PATCH",
             headers: {
                 "Content-type": "application/json",
@@ -81,7 +81,7 @@ function Project() {
         }
             project.cost = newCost
 
-            fetch(`http://localhost:5000/projects/${project.id}`,{
+            fetch(`https://json-cost.vercel.app/projects/${project.id}`,{
                 method: "PATCH",
                 headers: {
                     "Content-type" : "application/json"
@@ -106,7 +106,7 @@ function Project() {
         projectUpdated.services = servicesUpdated
         projectUpdated.cost = parseFloat(projectUpdated.cost) - parseFloat(cost)
 
-        fetch(`http://localhost:5000/projects/${projectUpdated.id}`, {
+        fetch(`https://json-cost.vercel.app/projects/${projectUpdated.id}`, {
             method: "PATCH",
             headers: {
                 "Content-Type": "application/json",
